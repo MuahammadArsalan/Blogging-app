c4cfa421c320abcd5ccde15883a1631088a75112
MuahammadArsalan <muhammadarsalanforever@gmail.com>
2024-09-25 13:37:26 -0700

    card done

--- src/assets/Screens/Dashboard.jsx
+++ src/assets/Screens/Dashboard.jsx
@@ -41,24 +41,58 @@ async function addBlog (data){
 return (
 
 < >
-<div style={{}} className='bg-base-300'>
+<div style={{width:"100vw",height:"400vh"}} className='bg-base-300'>
 
-  <h1>Dashboard</h1>
+  <h1 className='font-bold text-4xl p-6 text-center'>Dashboard</h1>
 
 
-  <form onSubmit={handleSubmit(onSubmit)}>
+  <form onSubmit={handleSubmit(onSubmit)} className='p-10 w-[60vw] m-auto h-[90vh] bg-white rounded-3xl '>
 
-  <input placeholder='Enter title' {...register("title" , { required: true })}
+  <input className='p-4 ml-28 w-[30rem] h-[3rem] mt-5' placeholder='Enter title'  style={{
+    border:"1px solid grey " ,
+    
+  }} {...register("title" , { required: true })}
   />  <br /> <br />
   {errors.exampleRequired && <span>This field is required</span>}
-  <input placeholder='Enter description' {...register("description" , { required: true })}
+  <input  placeholder='Whats in your mind' className='w-[30rem] h-[6rem] align-text-top p-4  ml-28' style={{
+    border:"1px solid grey"
+
+  }}  {...register("description" , { required: true })}
   />
   {errors.exampleRequired && <span>This field is required</span>}
 <br /> <br />
 
-<button className='btn btn-primary' onClick={addBlog}>Add Blog </button>
+<button className='btn btn-primary m-auto text-center ml-28 mt-1 w-[10rem]' onClick={addBlog}>Publish Blog </button>
 
 </form>
+
+
+
+
+
+<div>
+
+<h1  className='font-bold text-3xl mt-8 ml-72'>My Blogs</h1>
+
+
+
+<div className='w-[60vw]'  style={{
+  border:"1px solid grey",
+  backgroundColor:"white",
+  borderRadius:"15px",
+  marginLeft:"280px",
+  marginTop:"30px",
+  display:"inline-flex",
+  alignItems:"center",
+  flexWrap:"wrap"
+}}>
+  <p>aknakdnkndkan</p>
+  <p>Lorem, ipsum Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem rem ipsa harum eaque modi odio officia porro accusantium dolorem obcaecati. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur, minus voluptate. Porro sit cumque officia aliquam doloribus dicta quasi facilis!  dorepellat soluta architecto corporis. Distinctio tempora cumque quibusdam? Eaque, blanditiis beatae? Dolore aspernatur molestiae hic deserunt numquam vel reprehenderit aperiam quas itaque in minima consectetur, ex veniam et ratione ab sunt consequuntur recusandae!</p>
+</div>
+
+
+
+</div>
 
 
 </div>
