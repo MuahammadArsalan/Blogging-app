da9a584f386a57d453c3203d44983b50111abe9c
MuahammadArsalan <muhammadarsalanforever@gmail.com>
2024-09-25 23:32:23 -0700

    Data coming from firebase

--- src/assets/Screens/Dashboard.jsx
+++ src/assets/Screens/Dashboard.jsx
@@ -1,9 +1,35 @@
 import { data } from 'autoprefixer';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useForm } from "react-hook-form"
-import { getData, sendData } from '../Configuration/FirebaseMethod';
+import { auth, getData, sendData } from '../Configuration/FirebaseMethod';
+import { onAuthStateChanged } from 'firebase/auth';
 
 function Dashboard() {
+
+let [Blogs , setBlogs] = useState([])
+let [BlogsFromFireBase, setBlogsFromFireBase] = useState([])
+
+
+useEffect(() => {
+try {
+  
+  onAuthStateChanged(auth , async(user)=>{
+    if(user){
+      console.log(user.uid)
+      let uid = user.uid
+      const BlogsData = await getData("Blogs" , uid)
+      console.log(BlogsData)
+      setBlogs([...BlogsData])
+    }
+  })
+} catch (error) {
+  console.log(error);
+  
+}
+}, []);
+
+
+
   const {
     register,
     handleSubmit,
@@ -24,37 +50,70 @@ async function addBlog (data){
     let send = await sendData({
       title:data.title,
       description:data.description,
+      uid:auth.currentUser.uid
     } , 'Blogs')
-    console.log(send);
+    console.log(send)
 
-    let get =  await getData('Blog' ,uid)
-     console.log(get);
-    
-  } catch (error) {
+
+Blogs.push({
+  title:data.title,
+  description:data.description,
+})
+setBlogs([...Blogs])
+console.log(Blogs);
+
+
+
+// data.title = ''
+// data.description = ''
+
+// reset();
+} catch (error) {
     console.log(error);
     
   }
 
-
+  
+  data.title = ''
+  data.description = ''
 }
 
-return (
 
-< >
+// get data from firesstore
+
+// useEffect(() => {
+//   onAuthStateChanged (auth,async(user) => {
+//     if(user){
+//       console.log(user.uid);
+//      let data = await getData("Blogs" ,user.uid) 
+    
+//     console.log(data);
+    
+//     }
+//   })
+// } , [])
+
+
+
+
+
+return (
+  
+  < >
 <div style={{width:"100vw",height:"400vh"}} className='bg-base-300'>
 
   <h1 className='font-bold text-4xl p-6 text-center'>Dashboard</h1>
 
 
-  <form onSubmit={handleSubmit(onSubmit)} className='p-10 w-[60vw] m-auto h-[90vh] bg-white rounded-3xl '>
+  <form onSubmit={handleSubmit(onSubmit)} className='p-10 w-[60vw] m-auto h-[50vh] bg-white rounded-3xl '>
 
-  <input className='p-4 ml-28 w-[30rem] h-[3rem] mt-5' placeholder='Enter title'  style={{
+  <input className='p-4 ml-28 w-[30rem] h-[3rem] mt-5'   minLength={10} placeholder='Enter title'  style={{
     border:"1px solid grey " ,
     
   }} {...register("title" , { required: true })}
   />  <br /> <br />
   {errors.exampleRequired && <span>This field is required</span>}
-  <input  placeholder='Whats in your mind' className='w-[30rem] h-[6rem] align-text-top p-4  ml-28' style={{
+  <textarea  placeholder='Whats in your mind'  minLength="100" className='w-[30rem] h-[6rem] align-text-top p-4  ml-28' style={{
     border:"1px solid grey"
 
   }}  {...register("description" , { required: true })}
@@ -75,20 +134,33 @@ return (
 <h1  className='font-bold text-3xl mt-8 ml-72'>My Blogs</h1>
 
 
+{Blogs.length > 0 ? Blogs.map((item , index)=>{
 
-<div className='w-[60vw]'  style={{
-  border:"1px solid grey",
+return  <div className='w-[60vw]'  style={{
+  // border:"1px solid grey",
+  border:"none",
   backgroundColor:"white",
   borderRadius:"15px",
   marginLeft:"280px",
   marginTop:"30px",
   display:"inline-flex",
   alignItems:"center",
-  flexWrap:"wrap"
+  flexWrap:"wrap",
+  padding:"45px"
 }}>
-  <p>aknakdnkndkan</p>
-  <p>Lorem, ipsum Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem rem ipsa harum eaque modi odio officia porro accusantium dolorem obcaecati. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur, minus voluptate. Porro sit cumque officia aliquam doloribus dicta quasi facilis!  dorepellat soluta architecto corporis. Distinctio tempora cumque quibusdam? Eaque, blanditiis beatae? Dolore aspernatur molestiae hic deserunt numquam vel reprehenderit aperiam quas itaque in minima consectetur, ex veniam et ratione ab sunt consequuntur recusandae!</p>
+  <h1 className='font-semibold text-xl'>{item.title.toUpperCase() }</h1>
+  <br />
+  <p className='mt-3'>{item.description }</p>
+<div className='mt-5'>
+
+<button className='btn btn-primary w-[5rem] ml-0 h-[2rem]'>DELETE</button>
+<button className='btn btn-primary w-[5rem] ml-3 h-[2rem]'>EDIT</button>
 </div>
+
+</div>
+
+
+}): <h1  className='font-bold text-5xl p-6 text-center'>No Blogs Found...</h1> }
 
 
 
